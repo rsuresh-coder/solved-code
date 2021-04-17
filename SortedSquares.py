5760c37969a9b2c424cc52ab53169d84b9464b4a
rsuresh-coder <82728778+rsuresh-coder@users.noreply.github.com>
2021-04-18 01:23:12 +0530

    Create SortedSquares.py

---
+++ SortedSquares.py
@@ -0,0 +1,23 @@
+#Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+#Input: nums = [-4,-1,0,3,10]
+#Output: [0,1,9,16,100]
+
+#For example, with [-3, -2, -1, 4, 5, 6], we have the negative part [-3, -2, -1] with squares [9, 4, 1], and the positive part [4, 5, 6] with squares [16, 25, 36]. Our strategy is to iterate over the negative part in reverse, and the positive part in the forward direction.
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n - 1
+        for i in range(n - 1, -1, -1):
+            if abs(nums[left]) < abs(nums[right]):
+                square = nums[right]
+                right -= 1
+            else:
+                square = nums[left]
+                left += 1
+            result[i] = square * square
+        return result
+        
+        
