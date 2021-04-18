65bb12ae2bc85dbc6a2ea3db497f30c1ea8acb82
rsuresh-coder <82728778+rsuresh-coder@users.noreply.github.com>
2021-04-19 00:09:18 +0530

    Update duplicateZeroes.py

--- duplicateZeroes.py
+++ duplicateZeroes.py
@@ -11,6 +11,8 @@ Do the above modifications to the input array in place, do not return anything f
 Input: [1,0,2,3,0,4,5,0]
 After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
 
+https://leetcode.com/problems/duplicate-zeros/solution/
+
         """
 
         possible_dups = 0
